bfbb869d22873b670a8b943c53f16d809b0bc9e7
aqudei <fdeu.aalmine@gmail.com>
2022-06-24 10:37:04 +0800

    abc

--- bybit.py
+++ bybit.py
@@ -236,15 +236,15 @@ def main() -> None:
     tg_app.add_handler(MessageHandler(
         filters.TEXT & ~filters.COMMAND, handle_messages))
     ws_usdt_perpetual.order_stream(
-        lambda message:  asyncio.create_task(pybit_handle_message(message)))
+        lambda message:  loop.run_until_complete(pybit_handle_message(message)))
     ws_usdt_perpetual.execution_stream(
-        lambda message: asyncio.create_task(pybit_handle_message(message)))
+        lambda message: loop.run_until_complete(pybit_handle_message(message)))
     ws_usdc_perpetual.execution_stream(
-        lambda message: asyncio.create_task(pybit_handle_message(message)))
+        lambda message: loop.run_until_complete(pybit_handle_message(message)))
     ws_usdc_perpetual.order_stream(
-        lambda message: asyncio.create_task(pybit_handle_message(message)))
+        lambda message: loop.run_until_complete(pybit_handle_message(message)))
     ws_spot.execution_report_stream(
-        lambda message: asyncio.create_task(pybit_handle_message(message)))
+        lambda message: loop.run_until_complete(pybit_handle_message(message)))
 
     # Run the bot until the user presses Ctrl-C
     # We pass 'allowed_updates' handle *all* updates including `chat_member` updates
